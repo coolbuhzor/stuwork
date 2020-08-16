abc2bd7d68fc435ec191d723772a7af1d2d5df5d
buhzor <coolbuhzor@gmail.com>
2020-08-16 16:54:47 +0100

    first commit

---
+++ js-files/signup-app.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
